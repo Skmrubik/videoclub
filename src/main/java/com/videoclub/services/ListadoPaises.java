650a5b0cb0d6ef1566a614bb770eb80c5c23f90a
JORGE\jorge <skmrubik@gmail.com>
2025-07-22 17:05:54 +0200

    primera aproximacion

---
+++ src/main/java/com/videoclub/services/ListadoPaises.java
@@ -0,0 +1,24 @@
+package com.videoclub.services;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ListadoPaises {
+	
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
+	
+	@GetMapping("/listCountries")
+	public String hello() {
+	    String sql = "select * from country";
+	         
+	    List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
+	    return "h";
+	}
+}
