a2bea238a59111a41d82e476b59146bfe603536f
JORGE\jorge <skmrubik@gmail.com>
2025-08-08 10:44:41 +0200

    listar actores de una pelicula

--- src/main/java/com/videoclub/services/FilmActorService.java
+++ src/main/java/com/videoclub/services/FilmActorService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.videoclub.entities.FilmActor;
@@ -22,6 +23,19 @@ public class FilmActorService {
 		try {
 			List<FilmActor> listFilmActors = new ArrayList<>();
 			listFilmActors = filmActorRepository.findAll();
+			return new ResponseEntity<>(listFilmActors, HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println(e);
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping("/listActorsOfFilms")
+	public ResponseEntity<List<FilmActor>> listFilmActors(@RequestParam String idFilm) {
+		try {
+			int id = Integer.parseInt(idFilm);
+			List<FilmActor> listFilmActors = new ArrayList<>();
+			listFilmActors = filmActorRepository.getActorsOfFilm(id);
 			return new ResponseEntity<>(listFilmActors, HttpStatus.OK);
 		} catch (Exception e) {
 			System.out.println(e);
