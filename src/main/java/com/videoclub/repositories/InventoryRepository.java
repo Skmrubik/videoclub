fe744e3d41b86779cdb5ce01a70295a22c894eca
JORGE\jorge <skmrubik@gmail.com>
2025-08-14 13:28:14 +0200

    obtener peliculas disponibles

--- src/main/java/com/videoclub/repositories/InventoryRepository.java
+++ src/main/java/com/videoclub/repositories/InventoryRepository.java
@@ -1,9 +1,14 @@
 package com.videoclub.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.videoclub.entities.Inventory;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Integer>{
+	
+	@Query(value = "select count(*) as cantidad from (select inventory_id from inventory where film_id = ?1 "
+			+ "except select inventory_id from rental where return_date is null)", nativeQuery= true)
+	Integer peliculasDisponibles(Integer filmId);
 
 }
--- src/main/java/com/videoclub/services/InventoryService.java
+++ src/main/java/com/videoclub/services/InventoryService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.videoclub.entities.Inventory;
@@ -30,5 +31,16 @@ public class InventoryService {
 		}
 	}
 	
+	@GetMapping("/getAvailableFilms")
+	public ResponseEntity<Integer> getAvailableFilms(@RequestParam String filmId) {
+		try {
+			int id = Integer.parseInt(filmId);
+			Integer numAvailable = inventoryRepository.peliculasDisponibles(id);
+			return new ResponseEntity<>(numAvailable, HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println(e);
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
 }
