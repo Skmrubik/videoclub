6c864ef2fb7488542d9c043d9c97ea9891b4e966
JORGE\jorge <skmrubik@gmail.com>
2025-08-21 17:29:05 +0200

    distinto aplication properties para cada entorno

--- src/main/java/com/videoclub/services/CorsConfig.java
+++ src/main/java/com/videoclub/services/CorsConfig.java
@@ -1,4 +1,5 @@
 package com.videoclub.services;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,11 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableJpaAuditing
 public class CorsConfig implements WebMvcConfigurer {
+	
+	@Value("${path.localhost1}")
+	String localhost1;
 
+	@Value("${path.localhost2}")
+	String localhost2;
+	
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica CORS a todos los endpoints de la API
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173") // Orígenes permitidos
+                .allowedOrigins(localhost1, localhost2) // Orígenes permitidos
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*") // Permite todas las cabeceras
                 .allowCredentials(true) // Permite credenciales (cookies, tokens de auth, etc.)
